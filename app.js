2e68be37e3076d0b7fa4317b8c4a8a005db02c28
saurav-cp <saurav@cultrplus.com>
2024-05-28 22:41:42 +0530

    first commit

--- app.js
+++ app.js
@@ -1,23 +1,67 @@
 const express = require('express');
+const session = require("express-session");
+const Keycloak = require("keycloak-connect");
 
 const app = express();
 const PORT = 3000;
 
+/** Keyclock related configurations and utilities - START */
+const USER_ROLE = process.env.USER_ROLE || 'express-user';
+const ADMIN_ROLE = process.env.ADMIN_ROLE || 'express-admin';
+
+const kcConfig = {
+    clientId: process.env.AUTH_CLIENT_ID || 'secure-express-service',
+    bearerOnly: true,
+    serverUrl: process.env.AUTH_SERVER || 'http://localhost:8080',
+    realm: process.env.AUTH_REALM || 'master'
+};
+
+const memoryStore = new session.MemoryStore();
+
+Keycloak.prototype.accessDenied = function (request, response) {
+    response.status(401)
+    response.setHeader('Content-Type', 'application/json')
+    response.end(JSON.stringify({ status: 401, message: 'Unauthorized/Forbidden', result: { errorCode: 'ERR-401', errorMessage: 'Unauthorized/Forbidden' } }))
+}
+
+const keycloak = new Keycloak({ store: memoryStore }, kcConfig);
+
+function adminOnly(token, request) {
+    return token.hasRole(`realm:${ADMIN_ROLE}`) || token.hasRole(`realm:${USER_ROLE}`);
+}
+
+function isAuthenticated(token, request) {
+    return token.hasRole(`realm:${ADMIN_ROLE}`) || token.hasRole(`realm:${USER_ROLE}`);
+}
+
+/** Keyclock related configurations and utilities - END */
+
+
+app.use(session({
+    secret: process.env.APP_SECRET || 'BV&%R*BD66JH',
+    resave: false,
+    saveUninitialized: true,
+    store: memoryStore
+}));
+  
+app.use( keycloak.middleware() );
+
+
 app.get('/public', (req, res) => {
-    res.status(200).send({ 
-      'message':  "This is a public enpoint which can be accessed by anonymous users",
+    res.status(200).send({
+        'message': "This is a public enpoint which can be accessed by anonymous users",
     });
 })
 
-app.get('/secured', (req, res) => {
-    res.status(200).send({ 
-      'message':  "This is a secured enpoint which can be accessed by any authenticated user",
+app.get('/secured', [keycloak.protect(isAuthenticated)], (req, res) => {
+    res.status(200).send({
+        'message': "This is a secured enpoint which can be accessed by any authenticated user",
     });
 })
 
-app.get('/secured-admin', (req, res) => {
-    res.status(200).send({ 
-      'message':  "This is a secured enpoint which can be accessed only by any authenticated user with role admin",
+app.get('/secured-admin', [keycloak.protect(adminOnly)], (req, res) => {
+    res.status(200).send({
+        'message': "This is a secured enpoint which can be accessed only by any authenticated user with role admin",
     });
 })
 
